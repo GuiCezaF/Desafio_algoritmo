d820aad5522283a20dd3417b2af852a8cb8cc89f
Guilherme Cezarino Felipe <124943256+GuiCezaF@users.noreply.github.com>
2023-11-09 23:39:37 +0000

    correção

--- desafio.py
+++ desafio.py
@@ -1,12 +1,21 @@
+'''
+Alunos: Guilherme Cezarino, Felipe Zamariolli, Gabriel Eduardo
+'''
+
+
+
+
 import unittest
 from desafio_func import herdeiros
 
+
 Familia = [
+    
     {'nome': 'Eduardo', 'pai': None},
     {'nome': 'Lucas', 'pai': 'Eduardo'},
     {'nome': 'Pedro', 'pai': 'Lucas'},
     {'nome': 'João', 'pai': 'Pedro'},
-]
+] 
 
 
 def test():
